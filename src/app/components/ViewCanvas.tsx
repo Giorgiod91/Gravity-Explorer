37e72e87983b029ca92c6043ef142c5e180e1620
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-12-01 03:32:59 +0100

    basic movement for now

--- src/app/components/ViewCanvas.tsx
+++ src/app/components/ViewCanvas.tsx
@@ -1,13 +1,22 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
+import { useState } from "react";
 
 type Props = {};
 
+const EarthGravity = () => {};
+
 function ViewCanvas({}: Props) {
+  const [position, setPosition] = useState<[number, number, number]>([0, 0, 0]);
+
+  const moveObject = (event: React.MouseEvent) => {
+    setPosition([1, 1, 1]);
+  };
+
   return (
     <Canvas>
-      <mesh position={[0, 0, 0]}>
+      <mesh onClick={moveObject} position={position}>
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color="orange" />
       </mesh>
